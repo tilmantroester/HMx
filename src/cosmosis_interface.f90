cb2d1e9214dc5ffe0b772976965a20452d495262
Tilman Troester <tilman.troester@gmail.com>
2018-04-09 16:31:11 +0100

    Fixed T_WHIM parameter.

--- src/cosmosis_interface.f90
+++ src/cosmosis_interface.f90
@@ -136,7 +136,7 @@ function execute(block, config) result(status)
     status = datablock_get_double_default(block, halo_model_parameters_section, "Gamma", 1.18, HMx_config%cosm%Gamma)
     status = datablock_get_double_default(block, halo_model_parameters_section, "log10_M0", 14.08, log10_M0)
     status = datablock_get_double_default(block, halo_model_parameters_section, "Astar", 0.02, HMx_config%cosm%Astar)
-    status = datablock_get_double_default(block, halo_model_parameters_section, "log10_whim", 6.0, HMx_config%cosm%whim)
+    status = datablock_get_double_default(block, halo_model_parameters_section, "log10_whim", 6.0, log10_whim)
 
     HMx_config%cosm%M0 = 10**log10_M0
     HMx_config%cosm%whim = 10**log10_whim
